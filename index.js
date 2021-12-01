2f5cdefb8f5080c9548c8523fe833675cee8ce29
Valtteri Andström <valtteri.andstrom@gmail.com>
2021-12-01 18:37:39 +0200

    create and update users

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const { connectToDatabase } = require('./util/db')
 const middleware = require('./util/middleware')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(express.json())
 app.use(middleware.errorHandler)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const start = async () => {
   await connectToDatabase()

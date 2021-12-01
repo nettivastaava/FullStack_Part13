82668f2b713539eaab7d8b48a453755643abeeba
Valtteri Andström <valtteri.andstrom@gmail.com>
2021-12-01 11:52:22 +0200

    refactoring

---
+++ controllers/blogs.js
@@ -0,0 +1,31 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()  
+  res.json(blogs)
+})
+  
+router.post('/', async (req, res) => {
+  console.log(req.body)
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+  
+router.delete('/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+    
+  if (blog) {
+    blog.destroy()
+    res.status(204).end()
+  } else {
+    res.status(404).end()
+  }
+})
+
+module.exports = router

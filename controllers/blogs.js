0ac795b43b15a057e59b019e96b85f8f3f2292de
Valtteri Andström <valtteri.andstrom@gmail.com>
2021-12-01 12:23:55 +0200

    update likes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -28,4 +28,16 @@ router.delete('/:id', async (req, res) => {
   }
 })
 
+router.put('/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+
+  if (blog) {
+    blog.likes = req.body.likes
+    await blog.save()
+    res.json(blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
 module.exports = router
